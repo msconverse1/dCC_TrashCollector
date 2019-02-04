8d3787d166cf99de89ec35b9c5586ce3e0594320
msconverse1 <msconverse@fullsail.edu>
2019-02-04 10:33:32 -0600

    Main Project Setup

---
+++ MSC_dCC_TrashCollector/MSC_dCC_TrashCollector/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MSC_dCC_TrashCollector.MvcApplication" Language="C#" %>
---
+++ MSC_dCC_TrashCollector/MSC_dCC_TrashCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSC_dCC_TrashCollector.Startup))]
+namespace MSC_dCC_TrashCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
